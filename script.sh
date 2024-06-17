9061151ba84826d6cd6abcee9e317e4cb64112f5
vasim <aneeshaanu4578@gmail.com>
2024-06-17 17:56:30 +0530

    jjdasbkhk

--- script.sh
+++ script.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+a=$1
+b= $2
+scriptname=$0
+ALL_ARGUMENTS_PASSED=$*
+ALL_ARGUMENTS_PASSED1=$@
+NUMBER_OF_ARGUMENTS=$#
 
-read -p "please enter the name :" name
-
-
-
-
-echo my name is  $name
-
-
+echo "value of a is $a"
+echo "value of b is $b"
+echo "script name is $0"
+echo "arguments passed $ALL_ARGUMENTS_PASSED"
+echo "arguments passed $ALL_ARGUMENTS_PASSED1"
+echo "number of arguments passed $NUMBER_OF_ARGUMENTS"
 
 
 
