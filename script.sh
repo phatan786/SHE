433d5f9346833e957f8cb91188353463aff17f20
vasim <aneeshaanu4578@gmail.com>
2024-06-17 18:34:02 +0530

    jjdasbkhk

--- script.sh
+++ script.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
-a=$1
-b= $2
-scriptname=$0
-ALL_ARGUMENTS_PASSED=$*
-ALL_ARGUMENTS_PASSED1=$@
-NUMBER_OF_ARGUMENTS=$#
-
-echo "value of a is $a"
-echo "value of b is $b"
-echo "script name is $0"
-echo "arguments passed $ALL_ARGUMENTS_PASSED"
-echo "arguments passed $ALL_ARGUMENTS_PASSED1"
-echo "number of arguments passed $NUMBER_OF_ARGUMENTS"
+lsss -l
+exit 1
+echo "my name is vasim"
 
 
 
